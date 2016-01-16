7560d1c3f12e144aa63eed17e76e6758f53da47f
Joerg D <joerg@JDX230.router.dorgeist.net>
2016-01-16 18:10:26 +0100

    More Fading // Documentation changed to anglais

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
-Neopixel-Uhr
+Neopixel-Clock
 ============
 
-Simple Neopixel-Uhr
+Simple Neopixel-Clock
 
 --------
-Vorraussetzungen (HW):
+Prerequisites (HW):
 - Arduino (ATMEGA32u4)
-- RTC auf Basis DS3231 AT24c32 (VCC/GND, SDA auf PIN2, SCL auf PIN3)
-- 60 x WS2812B (Eigene 5V Versorgung! // Din auf PIN5)
+- RTC based on DS3231 AT24c32 (Connect: VCC/GND, SDA to PIN2, SCL to PIN3)
+- 60 x WS2812B (Own 5V Powersupply! // Connect Din to PIN5)
 
-Vorraussetzungen (SW):
+Prerequisites (SW):
 - FastLED-Library (http://fastled.io/)
 - Arduino IDE >1.6
 - WireRtcLib (https://github.com/akafugu/ds_rtc_lib)
--- clockwork_micro_rtc.ino
+++ clockwork_micro_rtc.ino
@@ -3,67 +3,67 @@
 // RTC
 #include <Wire.h>
 #include <WireRtcLib.h>
-WireRtcLib rtc; // RTC Objekt
+WireRtcLib rtc;                   // RTC Object
 
-#define DATA_PIN    5             // An PIN 5 haengt der Strip
-#define LED_TYPE    WS2812        // Wir haben es mit einem WS2812 zu tun
-#define COLOR_ORDER GRB           // Der die Farben in der Reihenfolge GRB entgegennimmt
-#define NUM_LEDS    60            // 60 LEDs sind es in Summe
-CRGB leds[NUM_LEDS];              // Ansteuern moechten wir die LEDs ueber das Array "leds"
+#define DATA_PIN    5             // Strip is attached to PIN5
+#define LED_TYPE    WS2812        // We have a WS2812
+#define COLOR_ORDER GRB           // Colorordering of the Strip is GRB 
+#define NUM_LEDS    60            // We have 60 LED's
+CRGB leds[NUM_LEDS];              // Our Control-Array is called "leds"
 
-#define BRIGHTNESS          42    // Konstante um die Helligkeit vorzugeben
-#define FRAMES_PER_SECOND  120    // Delays (s.u.) zwischen jedem Update
+#define BRIGHTNESS          42    // Brightness Constant
+#define FRAMES_PER_SECOND  120    // Delays (s.u.) between updates
 
-unsigned long spos=0; // Globale Var um die Sekunden zu halten.
-unsigned long o_spos=-1; // Globale Var um die Sekunden zu halten.
-unsigned long millis_offset=0;
+unsigned long spos=0;             // Global Var to hold seconds
+unsigned long o_spos=-1;          // Global Var to hold previous seconds
+unsigned long millis_offset=0;    // Global var to hold our calculated offset (see loop)
 
 void setup()
 {
-  delay(4000);  // Dem mC etwas Zeit zum einpendeln geben.
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // LEDs initialisieren
-  Wire.begin(); // i2c-Bus initialisieren (WICHTIG! Sonst tut die RTC nicht)
-  rtc.begin();  // RTC initialisieren
+  delay(4000);                    // Give the controler some Time to warm-up
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // initialize the LEDs
+  Wire.begin();                   // init the i2c-Bus (IMPORTANT! Without this, there's RTC-Trouble :)
+  rtc.begin();                    // RTC initi
 }
 
 
 void loop()
 { 
-  unsigned long spos=get_spos();  // RTC abfragen
-  if (o_spos != spos) {           // Neue Sekunde?
-    o_spos=spos;                  // Dann alte auf neue setzen
-    millis_offset=millis();       // internen Millisec.-Counter des mC in offset speichern (das ding ist sonst zu ungenau)
+  unsigned long spos=get_spos();  // Ask RTC for time
+  if (o_spos != spos) {           // New second?
+    o_spos=spos;                  // Set old to new
+    millis_offset=millis();       // save internal millisec.-Counter of the mC to offset 
   }
-  for (byte i=0;i<NUM_LEDS;i++) { // Einmal alle LEDs durchlaufen (fuer "Mark"-LEDs)
-    if (i%5 == 0) {               // Position durch 5 glatt teilbar?
-     // leds[i]=CRGB(2,2,0);      // Dann SET
+  for (byte i=0;i<NUM_LEDS;i++) { // Loop all LEDs once (for the "Mark"-LEDs)
+    if (i%5 == 0) {               // Position modulus 5 equals 0?
+     // leds[i]=CRGB(2,2,0);      // then SET
     }
-    if (i%15 == 0) {              // Position durch 15 glatt teilbar
-      leds[i]=CRGB(3,3,3);        // Dann SET
+    if (i%15 == 0) {              // Position modulus 15 equals 0?
+      leds[i]=CRGB(3,3,3);        // then SET
     }
   }
   
-  float hpos=(((float)spos/3600)*60/12);  // Stundenposition berechnen und auf die 60 Teilstriche (hier noch Fliesskommazahl) umrechnen
-  set_led((round(hpos+1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // Setze NACHlaufende Stunde gedimmt
-  set_led((round(hpos)%NUM_LEDS),BRIGHTNESS, 0, 0);                           // Setze Stunde
-  if (hpos==-1) { hpos=12; }                                                  // Rollover um 00:0 bzw. 12:00
-  set_led((round(hpos-1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // Setze VORlaufende Stunde
-  float nbright=((float)spos/60)-int(spos/60);               // Fadingeffekt fuer Minuten errechnen (Fraction of Minutes)
-  set_led(((spos/60))%NUM_LEDS,0, BRIGHTNESS-ceil(nbright*BRIGHTNESS), 0);    // Setze Minuten auf Helligkeit - (Helligkeit * Fraction)
-  set_led(((spos/60)+1)%NUM_LEDS,0, ceil(nbright*BRIGHTNESS), 0);             // Setze Minuten+1 auf Helligkeit * Fraction
-  nbright=(float)(millis()-millis_offset)/1000;         // Berechne Sekundenbruchteil
-  byte sekufraction=ceil(nbright*BRIGHTNESS);                 // Berechne aus dem Bruchteil Dimmfaktor * Helligkeit
-  if (sekufraction>BRIGHTNESS) { sekufraction=BRIGHTNESS; }   // Neuer Helligkeitskorrekturwert groesser als Max-Helligkeit? Dann begrenzen
-  set_led(spos%NUM_LEDS,0, 0, BRIGHTNESS-sekufraction); // Setze Sekunde gedimmt um korrekturwert
-  set_led((spos+1)%NUM_LEDS,0, 0, sekufraction);                   // Setze Zukunftssekunde gedimmt um korrekturwert
+  float hpos=(((float)spos/3600)*60/12);                                      // Calculate Hourposition for the 60 divisors as floatingpoint
+  set_led((round(hpos+1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // SET hour after the current (as dimmed)
+  set_led((round(hpos)%NUM_LEDS),BRIGHTNESS, 0, 0);                           // SET hour
+  if (hpos==-1) { hpos=12; }                                                  // Rollover at 00:00 / 12:00
+  set_led((round(hpos-1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // SET hour before the current (as dimmed)
+  float nbright=((float)spos/60)-int(spos/60);                                // Calculate fading for Minutes (Fraction)
+  set_led(((spos/60))%NUM_LEDS,0, BRIGHTNESS-ceil(nbright*BRIGHTNESS), 0);    // Set Minute to BRIGHTNESS - (BRIGHTNESS * Fraction)
+  set_led(((spos/60)+1)%NUM_LEDS,0, ceil(nbright*BRIGHTNESS), 0);             // Set Minute+1 to (BRIGHTNESS * Fraction)
   
-  FastLED.show();                                               // Und SHOWTime
-  FastLED.delay(1000/FRAMES_PER_SECOND);                        // Warten
-
-  fadeToBlackBy(leds,NUM_LEDS,5);                               // Alles was nicht neu gesetzt wurde ausfaden
+  nbright=(float)(millis()-millis_offset)/1000;                               // Do the same for the seconds, but based on our millis()
+  byte sekufraction=ceil(nbright*BRIGHTNESS);                                 
+  if (sekufraction>BRIGHTNESS) { sekufraction=BRIGHTNESS; }             
+  set_led(spos%NUM_LEDS,0, 0, BRIGHTNESS-sekufraction); 
+  set_led((spos+1)%NUM_LEDS,0, 0, sekufraction);
+  
+  FastLED.show();                                               // And SHOWTime
+  FastLED.delay(1000/FRAMES_PER_SECOND);                        // Wait
+  for(int i = 0; i < NUM_LEDS; i++) { leds[i] = CRGB(0,0,0); }
 }
 
-unsigned long get_spos() {  // Returned die aktuelle Zeit seit Mitternacht in Sekunden
+unsigned long get_spos() {  // Returns timeticks after midnight in seconds
   WireRtcLib::tm* t = rtc.getTime();
   return ( (long(t->hour%12)*3600)+(long(t->min)*60)+(t->sec) );
 }
@@ -74,7 +74,7 @@ void set_led (byte which_one, byte r,byte g, byte b) {
     if (g<=3) { g=3; }
     if (b<=3) { b=3; }
   }
-  leds[which_one]=CRGB(r,g,b);
+  leds[which_one] += CRGB(r,g,b); // Add new color to old one
 }
 
 
