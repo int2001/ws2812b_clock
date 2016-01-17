8187c7ad3031644772ab5974e233b1b995c4c500
Joerg D <joerg@JDX230.router.dorgeist.net>
2016-01-17 08:55:18 +0100

    Calculate fading via cosinus

--- clockwork_micro_rtc.ino
+++ clockwork_micro_rtc.ino
@@ -1,8 +1,12 @@
-#include "FastLED.h"
+/*
+ * Connect Data-In of Strip to PIN5
+ * Connect Power of WS2812 to own supply, don't forget to connect GND to mC AND Strip
+ * Connect RTC-SDA to PIN2, RTC-SCL to PIN3
+ */
+#include "FastLED.h"              // FastLED-Lib
 
-// RTC
-#include <Wire.h>
-#include <WireRtcLib.h>
+#include <Wire.h>                 // i2c-Lib
+#include <WireRtcLib.h>           // RTC-Lib
 WireRtcLib rtc;                   // RTC Object
 
 #define DATA_PIN    5             // Strip is attached to PIN5
@@ -23,7 +27,7 @@ void setup()
   delay(4000);                    // Give the controler some Time to warm-up
   FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip); // initialize the LEDs
   Wire.begin();                   // init the i2c-Bus (IMPORTANT! Without this, there's RTC-Trouble :)
-  rtc.begin();                    // RTC initi
+  rtc.begin();                    // RTC init
 }
 
 
@@ -35,46 +39,46 @@ void loop()
     millis_offset=millis();       // save internal millisec.-Counter of the mC to offset 
   }
   for (byte i=0;i<NUM_LEDS;i++) { // Loop all LEDs once (for the "Mark"-LEDs)
-    if (i%5 == 0) {               // Position modulus 5 equals 0?
-     // leds[i]=CRGB(2,2,0);      // then SET
-    }
     if (i%15 == 0) {              // Position modulus 15 equals 0?
       leds[i]=CRGB(3,3,3);        // then SET
     }
   }
   
+  // Hours
   float hpos=(((float)spos/3600)*60/12);                                      // Calculate Hourposition for the 60 divisors as floatingpoint
   set_led((round(hpos+1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // SET hour after the current (as dimmed)
   set_led((round(hpos)%NUM_LEDS),BRIGHTNESS, 0, 0);                           // SET hour
   if (hpos==-1) { hpos=12; }                                                  // Rollover at 00:00 / 12:00
   set_led((round(hpos-1)%NUM_LEDS),BRIGHTNESS/4, 0, 0);                       // SET hour before the current (as dimmed)
-  float nbright=((float)spos/60)-int(spos/60);                                // Calculate fading for Minutes (Fraction)
-  set_led(((spos/60))%NUM_LEDS,0, BRIGHTNESS-ceil(nbright*BRIGHTNESS), 0);    // Set Minute to BRIGHTNESS - (BRIGHTNESS * Fraction)
-  set_led(((spos/60)+1)%NUM_LEDS,0, ceil(nbright*BRIGHTNESS), 0);             // Set Minute+1 to (BRIGHTNESS * Fraction)
   
+  // Minutes
+  float nbright=((float)spos/60)-int(spos/60);                                // Calculate fading for Minutes (Fraction)
+  nbright=(float)1-(cos8((float)nbright*127)/(float)255);                     // Calculate cosinusvalue based on fraction for smoother fading (for only one pi)
+  byte fraction=ceil(nbright*BRIGHTNESS);                                     // Save our new fraction
+  if (fraction>BRIGHTNESS) { fraction=BRIGHTNESS; }                           // Result greater than max-brightness? Cut it to max-brightness
+  set_led(((spos/60))%NUM_LEDS,0, BRIGHTNESS-fraction, 0);                    // Set Minute to BRIGHTNESS - (BRIGHTNESS * Fraction)
+  set_led(((spos/60)+1)%NUM_LEDS,0, fraction, 0);                             // Set Minute+1 to (BRIGHTNESS * Fraction)
+
+  // Seconds
   nbright=(float)(millis()-millis_offset)/1000;                               // Do the same for the seconds, but based on our millis()
-  byte sekufraction=ceil(nbright*BRIGHTNESS);                                 
-  if (sekufraction>BRIGHTNESS) { sekufraction=BRIGHTNESS; }             
-  set_led(spos%NUM_LEDS,0, 0, BRIGHTNESS-sekufraction); 
-  set_led((spos+1)%NUM_LEDS,0, 0, sekufraction);
+  nbright=(float)1-(cos8((float)nbright*127)/(float)255);
+  fraction=ceil(nbright*BRIGHTNESS);                                 
+  if (fraction>BRIGHTNESS) { fraction=BRIGHTNESS; }                           // Result greater than max-brightness? Cut it to max-brightness
+  set_led(spos%NUM_LEDS,0, 0, BRIGHTNESS-fraction); 
+  set_led((spos+1)%NUM_LEDS,0, 0, fraction);
   
   FastLED.show();                                               // And SHOWTime
   FastLED.delay(1000/FRAMES_PER_SECOND);                        // Wait
-  for(int i = 0; i < NUM_LEDS; i++) { leds[i] = CRGB(0,0,0); }
+  for(int i = 0; i < NUM_LEDS; i++) { leds[i] = CRGB(0,0,0); }  // Set all LEDs off
 }
 
-unsigned long get_spos() {  // Returns timeticks after midnight in seconds
+unsigned long get_spos() {                                      // Returns timeticks after midnight in seconds
   WireRtcLib::tm* t = rtc.getTime();
   return ( (long(t->hour%12)*3600)+(long(t->min)*60)+(t->sec) );
 }
 
 void set_led (byte which_one, byte r,byte g, byte b) {
-  if (which_one % 15 == 0) {  // This is a "Mark"-LED
-    if (r<=3) { r=3; }
-    if (g<=3) { g=3; }
-    if (b<=3) { b=3; }
-  }
-  leds[which_one] += CRGB(r,g,b); // Add new color to old one
+  leds[which_one] += CRGB(r,g,b);                               // Add new color to old one
 }
 
 
